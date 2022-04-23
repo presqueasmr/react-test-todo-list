1a72cbc4af431fc8b16d3cb8d55d0bfb2252f14c
presqueasmr <auriane1103@gmail.com>
2022-04-23 10:25:37 +0100

    todo: edit task

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,17 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+
   const taskList = tasks.map(task => (
     <Todo 
       id={task.id} 
@@ -30,6 +41,7 @@ function App(props) {
       completed={task.completed}
       key={task.id}
       deleteTask={deleteTask}
+      editTask={editTask}
       />));
 
   return ( 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo(props){
-    return (
-    <li>
+    const [isEditing, setEditing] = useState(false);
+    const [newName, setNewName] = useState('');
+
+    function handleChange(e) {
+        setNewName(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.editTask(props.id, newName);
+        setNewName("");
+        setEditing(false);
+    }
+      
+    const editingTemplate = (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor={props.id}>
+              New name : 
+            </label>
+            <input id={props.id} type="text" value={newName} onChange={handleChange} />
+          </div>
+          <div className="btn-group">
+          <button type="button" className="btnCancel"
+            onClick={() => setEditing(false)}>
+            Cancel
+        </button>
+
+            <button type="submit" className="btnEdit">
+              Save
+            </button>
+          </div>
+        </form>
+      );
+
+      const viewTemplate = (
         <div>
-            <input id={props.id} type="checkbox" defaultChecked={props.completed} />
-            <label className="toDoLabel" htmlFor={props.id}>{props.name}</label>
+          <div>
+              <input id={props.id} type="checkbox" defaultChecked={props.completed}/>
+              <label htmlFor={props.id}>
+                {props.name}
+              </label>
+            </div>
+            <div className="btn-group">
+            <button type="button" className="btnEdit" onClick={() => setEditing(true)}>
+            Edit 
+            </button>
+
+              <button
+                type="button"
+                className="btnDelete"
+                onClick={() => props.deleteTask(props.id)}>
+                Delete
+              </button>
+            </div>
         </div>
-        <div>
-            <button type='button'>Edit</button>
-            <button type="button" className="btnDelete"
-            onClick={() => props.deleteTask(props.id)}>Delete</button>
-        </div>
-    </li>
-    );
+      );
+
+    return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
