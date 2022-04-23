c46c6d98a4ba4ad4ae835389eb5864a4352ab504
presqueasmr <auriane1103@gmail.com>
2022-04-23 09:22:11 +0100

    create react app gestion tâches

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import '../styles/App.css';
+
+
+
+function App(props) {
+  return ( 
+    <div className="toDoList">
+      <h1>To Do List de Auriane</h1>
+    </div>
+)
+}
+
+
+export default App;
